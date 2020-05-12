af6a6a921584192839444c9a38537366f22eff63
mattm <mathstorey@gmail.com>
2020-05-13 10:18:52 +1200

    file input fixed

--- normalisation.py
+++ normalisation.py
@@ -10,9 +10,9 @@ import sys
 #!{sys.executable} -m pip install xlrd
 
 ## Get most recent uploaded input file from plate reader 
-#list_of_xlsx_files = glob.glob('/root/plateReaderData/*.xlsx') # will need path of where these are on the robot file system
+list_of_xlsx_files = glob.glob('/root/plateReaderData/*.xlsx') # will need path of where these are on the robot file system
 
-list_of_xlsx_files = glob.glob('*.xlsx') # will need path of where these are on the robot file system
+#list_of_xlsx_files = glob.glob('*.xlsx') # will need path of where these are on the robot file system
 
 
 #latest_file = "/root/plateReaderData/pciogreen_pcr-20200414-xr.xlsx"
